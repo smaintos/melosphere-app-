e9a02ecffac5fc25683cfb7476c5c2c810e4f821
smaintos <william.mariamon@ynov.com>
2024-03-23 01:14:50 +0100

    front avanced

--- melosphere/src/Pages/accueil.js
+++ melosphere/src/Pages/accueil.js
@@ -8,7 +8,7 @@ function Accueil() {
         <NavBar />
         <div className="bg-gray-100 h-screen">
           <div className="m-0 flex justify-center items-center pt-24">
-            <h1 className="text-4xl">Youtube : La musique en mieux </h1>
+          <h1 className="text-4xl lg:text-5xl">Youtube : La musique en mieux </h1>
           </div>
         </div> 
       </header>
